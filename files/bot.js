74a4cc96308c54734d8d614a7a5ff3091d25f42d
Unknown807 <vocabhelp1@gmail.com>
2020-09-11 18:43:51 +0100

    Added simple message levelling system

--- files/bot.js
+++ files/bot.js
@@ -1,14 +1,15 @@
-const { Client, Util, MessageEmbed } = require("discord.js");
+const ds = require("discord.js");
+const fs = require("fs");
 const { TOKEN, PREFIX } = require("./config");
 
-const client = new Client({ disableEveryone: true });
+const client = new ds.Client({ disableEveryone: true });
 
 client.on("warn", console.warn);
 client.on("error", console.error);
 
 client.on("ready", () => {
   console.log("READY")
-  client.user.setActivity("Not doing much...", {type: "WATCHING"})
+  client.user.setActivity("Time Fly...", {type: "WATCHING"})
 });
 
 client.on("disconnect", () => console.log("DISCONNECTED"));
@@ -20,6 +21,10 @@ client.on("guildMemberAdd", member => {
     if (!channel) return;
     channel.send(`Welcome to the server, ${member}`);
 });
+
+client.on("guildMemberRemove", member => {
+    console.log(`${member.tag} has left the guild`)
+})
 
 function kickOrBan(msg, option) {
     let mentioned_user = msg.mentions.users.first();
@@ -38,7 +43,43 @@ function kickOrBan(msg, option) {
 
 client.on("message", async msg => { 
 	if (msg.author.bot) return undefined;
-	if (!msg.content.startsWith(PREFIX)) return undefined;
+	if (!msg.content.startsWith(PREFIX)) {
+        fs.readFile("userdata.json", (err, data) => {
+            if (err) {
+                console.log("ERROR READING FILE, FILE WILL BE CREATED");
+                let emptydata = JSON.stringify({});
+                fs.writeFile("userdata.json", emptydata, (err, _) => {
+                    if (err) console.log("ERROR", err);
+                });
+                return undefined;
+            }
+            let userdata = JSON.parse(data);
+            let user = msg.author.id;
+            console.log(userdata);
+            if (userdata.hasOwnProperty(user)) {
+                console.log("User Exists");
+                let currentlevel = userdata[user]["level"];
+                let currentexp = userdata[user]["exp"];
+                let currentmax = userdata[user]["max"];
+                if (currentexp+5 == currentmax) {
+                    userdata[user] = {"level":currentlevel+1,
+                            "exp":0, "max":currentmax*2};
+                    msg.reply(`Well done! You are now level ${currentlevel+1}`);
+                } else {
+                    userdata[user]["exp"] += 5;
+                }
+            } else {
+                console.log("User Doesn't Exist");
+                userdata[user] = {"level":0, "exp":5, "max":100};
+            }
+            let newdata = JSON.stringify(userdata);
+            fs.writeFile("userdata.json", newdata, (err, _) => {
+                if (err) console.log("ERROR", err); 
+            });
+            console.log(newdata);
+        });
+        return undefined;
+    }
 
 	let command = msg.content.toLowerCase().split(" ")[0];
     command = command.slice(PREFIX.length);
@@ -46,6 +87,16 @@ client.on("message", async msg => {
     switch (command) {
         case "avatar":
             msg.reply(msg.author.displayAvatarURL());
+            break;
+        case "mylevel":
+            fs.readFile("userdata.json", (err, data) => {
+                if (err){
+                    console.log("ERROR", err);
+                } else {
+                    let userlevel = JSON.parse(data)[msg.author.id]["level"];
+                    msg.reply(`You are level ${userlevel}`);
+                }
+            });
             break;
         case "kick":
             let kickmember = kickOrBan(msg, "kick");
